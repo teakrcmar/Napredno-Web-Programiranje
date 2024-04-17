cc32682a1e0cec2f8cfa373dbaae27106afd1a60
Tea <74956679+teakrcmar@users.noreply.github.com>
2024-04-17 09:30:50 +0200

    Delete LV5/radovi directory

--- LV5/radovi/resources/lang/en/tasks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'name' => 'Name',
-    'name_en' => 'Name (English)',
-    'description' => 'Description',
-    'study_type' => 'Study type',
-];
--- LV5/radovi/resources/lang/hr/tasks.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'name' => 'Naziv',
-    'name_en' => 'Naziv (engleski)',
-    'description' => 'Opis',
-    'study_type' => 'Vrsta studija',
-];
